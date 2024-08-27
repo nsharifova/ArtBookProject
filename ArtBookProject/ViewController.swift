9ba9295b1cb7cf2dba502871183ae6ca4dfe9b75
nsharifova <nurluay00@bk.ru>
2024-08-27 10:11:15 +0400

    Initial Commit

---
+++ ArtBookProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtBookProject
+//
+//  Created by Nurluay Sharifova on 27.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
